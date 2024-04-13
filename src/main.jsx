a1019b81ed6ae6cb32d2c6288439bae6426f1629
J-CamiloG <jc6381878@gmail.com>
2024-04-13 07:16:05 -0500

    Inicializacion del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import CardTwitter from './CardTwitter'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <>
+    <CardTwitter isFollowing  name='fernantorres' username='fitter'  />
+    <CardTwitter isFollowing={false} name='J-CamiloG' username='juan camilo campillo'  />
+    <CardTwitter name='carlos' username='carlos'  />
+  </>
 )
